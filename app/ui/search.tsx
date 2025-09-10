cd7cfe191b3e7805dc51010072e75bb5a22aa12a
Harriet Blundell <harriet.blundell27@hotmail.co.uk>
2025-09-10 15:31:01 +0100

    Logic from Chapter 11 - Adding Search and Pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,14 +2,16 @@
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParam = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
-    const params = new URLSearchParams(searchParam);
+  const handleSearch = useDebouncedCallback((term: string) => {
+    const params = new URLSearchParams(searchParam); // A Web API that provides utlities to manipulate the URL query parameters
+    params.set("page", "1");
 
     if (term) {
       params.set("query", term);
@@ -17,7 +19,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete("query");
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
