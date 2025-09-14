19a87c68485633e668cdaca80e0cb9c1e82070e4
Harriet Blundell <harriet.blundell27@hotmail.co.uk>
2025-09-14 16:46:34 +0100

    Handling errors

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -28,10 +28,14 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0]; // Get current date in YYYY-MM-DD format
 
-    await sql`
+    try {
+        await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+    } catch (error) {
+        console.error(error);
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
@@ -49,20 +53,27 @@ export async function updateInvoice(id: string, formData: FormData) {
 
     const amountInCents = amount * 100;
 
-    await sql`
+    try {
+        await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
-
+    } catch (error) {
+        console.error(error);
+    }
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
 
 // Deleting an Invoice with ID
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath('/dashboard/invoices');
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+    } catch (error) {
+        console.error(error);
+    }
+    revalidatePath('/dashboard/invoices');
 }
 
 // By using 'use server', I've marked all the exported functions within the file as Server Actions
